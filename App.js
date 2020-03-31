3dd6f458fa650608cc63f7e12fff71c99012eeae
Tolits Delizo <angbagongako@gmail.com>
2020-03-31 13:15:43 +0800

    Navigate between screents -- initital build

--- App.js
+++ App.js
@@ -1,33 +1,66 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { 
   StyleSheet, 
   Text, 
   View,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
+  Button
 } from 'react-native';
 
-export default function App() {
+function HomeScreen({ navigation } ) {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <View style={styles.logocontainer}>
-          <Image source={require('./assets/logo.png')} style={styles.logo} />
-        </View>
-        <View style={styles.buttoncontainer}>
-          <TouchableOpacity style={styles.btnsignup}>
-            <Text style={styles.btnsignuptext}>SIGN UP</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.btnlogin}>
-            <Text style={styles.btnlogintext}>LOGIN</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      </NavigationContainer>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Home... again"
+        onPress={() => navigation.push('Home')}
+      />
+      <Button title="Go to Welcome Screen" onPress={() => navigation.navigate('Welcome')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+          title="Go back to first screen in stack"
+          onPress={() => navigation.popToTop()}
+        />
+    </View>
   );
 }
+
+function WelcomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.logocontainer}>
+        <Image source={require('./assets/logo.png')} style={styles.logo} />
+      </View>
+      <View style={styles.buttoncontainer}>
+        <TouchableOpacity style={styles.btnsignup}>
+          <Text style={styles.btnsignuptext}>SIGN UP</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnlogin} onPress={() => navigation.navigate('Home')}>
+          <Text style={styles.btnlogintext}>LOGIN</Text>
+        </TouchableOpacity>        
+      </View>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={WelcomeScreen} options={{headerShown: false}} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
