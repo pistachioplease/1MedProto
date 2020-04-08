95a8982a28ede9568ca725f765c17a4215190b35
Tolits Delizo <angbagongako@gmail.com>
2020-04-08 12:19:18 +0800

    Add doctor second commit

--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -8,7 +8,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyCQWfZuzO8mPt-_kuNVK6Y2jqncwcPicg4",
   authDomain: "authflow-b1864.firebaseapp.com",
   databaseURL: "https://authflow-b1864.firebaseio.com/",
-  storageBucket: "authflow-b1864.appspot.com"
+  projectId: "authflow-b1864",
+  storageBucket: "authflow-b1864.appspot.com",
+  messagingSenderId: "219133059115",
+  appId: "1:219133059115:web:f71d77c4486a194679ca6e",
+  measurementId: "G-EZNWHB2YP8"
 };
 
 firebase.initializeApp(firebaseConfig);
